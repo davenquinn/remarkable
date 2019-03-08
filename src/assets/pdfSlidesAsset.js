d7a4c496f1229397232979860ce217f0aa67cba1
Daven Quinn <dev@davenquinn.com>
2019-03-07 22:42:13 -0600

    Updated pdf slide asset

---
+++ src/assets/pdfSlidesAsset.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const Asset = require('parcel-bundler/src/Asset');
+
+class PDFSlidesAsset extends Asset {
+  constructor(name, options) {
+    super(name, options);
+    this.type = 'json';
+    this.hmrPageReload = true;
+  }
+
+  async generate() {
+    const {parseSlides} = require('remark');
+    const compiled = parseSlides(this.contents);
+    console.log(compiled);
+    return [
+      {
+        type: 'json',
+        value: JSON.stringify(compiled)
+      }
+    ];
+  }
+}
+
+module.exports = PDFSlidesAsset;
+
